4ddae24c58958ed4cbd285d4be9d08808e7ed522
Muhammed Qureshi <119445359+MuhammedQureshi@users.noreply.github.com>
2023-06-23 23:58:55 +0100

    FetchUpd

--- client/script.js
+++ client/script.js
@@ -81,7 +81,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://aiconverse-o2aq.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
